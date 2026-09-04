94a777d43a9b51b541794b18aba5c7d1a885e5b3
DDRNFinch <dfinch1984@hotmail.co.uk>
2026-09-04 10:04:51 +0100

    Use refined v7 generator through v5 build entry

--- question-banks/question-bank-engine-v5.js
+++ question-banks/question-bank-engine-v5.js
@@ -1,6 +1,7 @@
 (()=>{"use strict";
 if(typeof require!=="function")throw new Error("Node build loader required for Naxos question-bank engine v5.");
 require("./question-bank-engine-v6.js");
-if(!globalThis.NaxosQuestionBankV6?.build)throw new Error("NaxosQuestionBankV6 did not initialise.");
-globalThis.NaxosQuestionBankV5=globalThis.NaxosQuestionBankV6;
+require("./question-bank-engine-v7.js");
+if(!globalThis.NaxosQuestionBankV7?.build)throw new Error("NaxosQuestionBankV7 did not initialise.");
+globalThis.NaxosQuestionBankV5=globalThis.NaxosQuestionBankV7;
 })();
